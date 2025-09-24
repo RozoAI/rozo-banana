9660783435c1b0ec396ee442df087fc1a1b86547
akbarsaputrait <akbarsaputrait@outlook.com>
2025-09-25 00:52:09 +0700

    chore: upgrade sdk version

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,5 +1,20 @@
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
+import { coinbaseWallet, phantomWallet } from "@rainbow-me/rainbowkit/wallets";
 import { getDefaultConfig as getDefaultConfigRozo } from "@rozoai/intent-pay";
 import { createConfig } from "wagmi";
+
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: "Recommended",
+      wallets: [phantomWallet, coinbaseWallet],
+    },
+  ],
+  {
+    appName: "Banana DApp",
+    projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string,
+  }
+);
 
 export const rozoPayConfig = createConfig(
   getDefaultConfigRozo({
@@ -9,5 +24,6 @@ export const rozoPayConfig = createConfig(
       typeof window !== "undefined"
         ? window.location.origin
         : "http://localhost:3000",
+    connectors: [...connectors],
   })
 );
